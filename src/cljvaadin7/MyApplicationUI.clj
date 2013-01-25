1241fb03f7376c3a666386e9ad012d56abe9a4c0
Tobias Bayer <tobayer@googlemail.com>
2013-01-25 14:53:43 +0100

    Add button and action

--- src/cljvaadin7/MyApplicationUI.clj
+++ src/cljvaadin7/MyApplicationUI.clj
@@ -1,13 +1,37 @@
 (ns cljvaadin7.MyApplicationUI
-  (import [com.vaadin.ui VerticalLayout Label])
+  (import [com.vaadin.ui VerticalLayout Label Button Button$ClickListener Notification])
   (:gen-class
     :extends com.vaadin.ui.UI))
+
+
+(defn- show-click-message
+  []
+  (Notification/show "Button clicked"))
+
+(defn- create-button-click-listener [action]
+  (reify Button$ClickListener 
+            (buttonClick 
+              [_ evt]
+              (action))))
+
+(defn- addAction
+  [button action]
+  (.addListener button (create-button-click-listener action)))
 
 (defn -init
   [this request]
   (let [layout (VerticalLayout.)
-        label (Label. "Hello Clojure-Vaadin 7!")]
+        label (Label. "Hello Clojure-Vaadin 7!")
+        button (Button. "Push me!")]
+    (addAction button show-click-message)
     (.addComponent layout label)
+    (.addComponent layout button)
     (.setContent this layout)))
+
+  
+
+
+
+
 
 
