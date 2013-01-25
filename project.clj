d76ca54e464f9cdb68bbf2ab7c6abb6ce77c6ba5
Tobias Bayer <tobayer@googlemail.com>
2013-01-25 13:13:36 +0100

    Add project description

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject cljvaadin7 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Demonstrates how to integrate Clojure and Vaadin 7"
+  :url "https://github.com/codebrickie/cljvaadin7"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.vaadin/vaadin-server "7.0.0.rc1"]
                  [com.vaadin/vaadin-client-compiled "7.0.0.rc1"]
                  [com.vaadin/vaadin-themes "7.0.0.rc1"]
-                 [javax.servlet/servlet-api "2.5"]] ;FIXME provided scope?
+                 [javax.servlet/servlet-api "2.5"]]
   :aot [cljvaadin7.MyApplicationUI])
 
