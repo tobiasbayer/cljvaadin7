aa09f9bb6aa3c494a96da2d4f88388546a03de72
Tobias Bayer <tobayer@googlemail.com>
2013-02-12 22:13:32 +0100

    Upgrade to Vaadin 7.0.0 final

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.vaadin/vaadin-server "7.0.0.rc1"]
-                 [com.vaadin/vaadin-client-compiled "7.0.0.rc1"]
-                 [com.vaadin/vaadin-themes "7.0.0.rc1"]
+                 [com.vaadin/vaadin-server "7.0.0"]
+                 [com.vaadin/vaadin-client-compiled "7.0.0"]
+                 [com.vaadin/vaadin-themes "7.0.0"]
                  [javax.servlet/servlet-api "2.5"]]
   :aot [cljvaadin7.MyApplicationUI])
 
